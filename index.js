1796c0389f1c038d699699605779aab26251cf66
Hakan Yildiz <hakanyildiz248@gmail.com>
2019-07-07 16:54:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 /* Your code goes here */
+ customerName = 'bob';
+
+function upperCaseCustomerName() {
+  customerName = 'BOB';
+}
+
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+
+function overWriteBestCustomer() {
+  bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'hakan';
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'denno';
+}
+ 
+
+function attemptTwoFavoriteCustomers() {
+  
+}
