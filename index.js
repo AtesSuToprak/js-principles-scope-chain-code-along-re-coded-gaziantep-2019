a70b8bc459f3b3585db47971975d63df0bbdda50
Hakan Yildiz <hakanyildiz248@gmail.com>
2019-07-07 17:17:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,8 +19,7 @@ function changeLeastFavoriteCustomer() {
   leastFavoriteCustomer = 'denno';
 }
 
-const favoriteCustomer = 'joe';
 function attemptTwoFavoriteCustomers() {
- let favoriteCustomer = 'Cal';
+  let favoriteCustomer = 'Hakan';
+  let favoriteCustomer = 'Cal';
 }
-
